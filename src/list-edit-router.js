2d7f32653305fe2a83ff9168c416ca43fef45538
Yerlis09 <castellaryerlis18@gmail.com>
2023-10-05 12:36:20 -0500

    se crea el metodo post con su respectiva ruta para crear

--- src/list-edit-router.js
+++ src/list-edit-router.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const listEditRouter = express.Router();
+
+listEditRouter.post('/create', (req, res) => {
+    // Aquí se obtiene los datos de la nueva tarea desde el cuerpo de la solicitud (req.body)
+    const newTask = req.body;
+    // Luego, agrega la tarea a la lista de tareas (tasks)
+    // Supongamos que tienes un array llamado 'tasks' con todas las tareas
+    // También puedes asignar un nuevo ID a la tarea si es necesario
+    tasks.push(newTask);
+    res.json(newTask);
+  });
+  
