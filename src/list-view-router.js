3f0e77c323199f533fdcda95b372771606cc06e2
Yerlis09 <castellaryerlis18@gmail.com>
2023-10-05 12:30:11 -0500

    se crea una solicitud GET con su respectiva ruta

--- src/list-view-router.js
+++ src/list-view-router.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const listViewRouter = express.Router();
+
+listViewRouter.get('/completed', (req, res) => {
+    // Aquí debes obtener y enviar las tareas completas desde la lista de tareas
+    // Supongamos que tenemos un array llamado 'tasks' con todas las tareas
+  
+    const completedTasks = tasks.filter((task) => task.isCompleted === true);
+    res.json(completedTasks);
+  });
+  
